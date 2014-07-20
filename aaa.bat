920989ff2133dffc3dbd90912942572c2d595f64
Demo1 <Demo1@demo>
2014-07-20 09:35:35 +0900

    update

--- aaa.bat
+++ aaa.bat
@@ -1,3 +1,3 @@
 cd %1
 rem "c:\Program Files (x86)\Git\bin\git.exe" add .
-"c:\Program Files (x86)\Git\bin\git.exe" commit -a -m "%2%3"
+"c:\Program Files (x86)\Git\bin\git.exe" commit -a -m "update"
