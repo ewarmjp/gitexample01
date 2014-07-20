9b41c46598f07dc672aefcd472c82810cd2babc8
daemon <aaa@aaa>
2014-07-20 10:41:11 +0900

    hhh

--- aaa.bat
+++ aaa.bat
@@ -1,3 +1,5 @@
+C:
 cd %1
 rem "c:\Program Files (x86)\Git\bin\git.exe" add .
-"c:\Program Files (x86)\Git\bin\git.exe" commit -a -m "update"
+"c:\Program Files (x86)\Git\bin\git.exe" commit -a -m "hhh"
+rem git.exe   status > C:\Tmp\S140720a_GITsample\xxxxxa.txt 2>&1
